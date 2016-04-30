11774475a746a7272528c33388bcf2447e896776
shilpi230 <shilpi@skcript.com>
2016-04-30 12:56:38 +0530

    Fixing global variable bug

--- watchman/__init__.py
+++ watchman/__init__.py
@@ -6,7 +6,7 @@ import logging
 import subprocess
 from watch import FileWatch
 from threading import Thread
-from watchman.conf import LOG_FILENAME, load_regexes, load_config
+from watchman.conf import LOG_FILENAME, load_regexes, load_config, update_config
 
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 log = logging.getLogger("watchman.init")
@@ -23,9 +23,7 @@ class Watchman():
             if len(config['source']) == 0:
                 Watchman.get_source(config)
 
-            yaml.dump(config, open(CONFIG_FILE, "w"), default_flow_style=False)
-
-            log.info("Config written in %s" % CONFIG_FILE)
+            update_config(config)
         except KeyboardInterrupt:
             log.error("Config cancelled by user")
 
@@ -35,12 +33,11 @@ class Watchman():
         if os.path.exists(source):
             print("Adding ({0})...".format(source))
             config['source'].append(source)
+            ans = raw_input("Add more paths? (y/n): ")
+            if ans in {"y", "Y"}:
+                Watchman.get_source(config)
         else:
             print("Path does not exist!")
-            Watchman.get_source(config)
-
-        ans = raw_input("Add more paths? (y/n): ")
-        if ans in {"y", "Y"}:
             Watchman.get_source(config)
 
     @staticmethod
