10f01dbd4aba1bbf74d7b28b0f56335508f76435
Swaathi K <swaathi@skcript.com>
2016-04-30 17:08:29 +0530

    Configurable API endpoints + better regex management

--- watchman/conf.py
+++ watchman/conf.py
@@ -10,23 +10,44 @@ LOG_FILENAME = 'watchman.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 log = logging.getLogger("watchman.conf")
 
-REGEX_FILE = "regexes.yml"
 CONFIG_FILE = os.path.expanduser("~/watchman.yml")
 
-def load_regexes():
-    if os.path.isfile(REGEX_FILE):
+def load_paths():
+    """ Try to load paths. """
+    if not os.path.isfile(CONFIG_FILE):
         return []
 
-    regexes = yaml.load(open(REGEX_FILE))
-    if isinstance(regexes, list):
-        return []
+    paths = yaml.load(open(CONFIG_FILE))['source']
+    if isinstance(paths, list):
+        return paths
     else:
-        return regexes
+        return []
 
+def load_regexes():
+    """ Try to load regexes. """
+    if not os.path.isfile(CONFIG_FILE):
+        return []
+
+    regexes = yaml.load(open(CONFIG_FILE))['regexes']
+    if isinstance(regexes, list):
+        return regexes
+    else:
+        return []
+
+def load_endpoints():
+    """ Try to load regexes. """
+    if not os.path.isfile(CONFIG_FILE):
+        return {}
+
+    endpoints = yaml.load(open(CONFIG_FILE))['endpoints']
+    if isinstance(endpoints, dict):
+        return endpoints
+    else:
+        return {}
 
 def load_config(config_file=CONFIG_FILE):
     """ Try to load YAML config file. """
-    config = {'source': []}
+    config = {'source': [], 'regexes': [], 'endpoints': {}}
     if os.path.isfile(config_file):
         log.debug("Try loading config file: {0}".format(config_file))
         config = yaml.load(open(config_file)) or config
@@ -37,5 +58,6 @@ def load_config(config_file=CONFIG_FILE):
     return config
 
 def update_config(config, config_file=CONFIG_FILE):
+    """ Try to update YAML config file. """
     yaml.dump(config, open(config_file, "w"), default_flow_style=False)
     log.info("Updated config in %s" % CONFIG_FILE)
