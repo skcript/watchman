84538550bdd60316fa22f395b99cddebdfc5bc1d
Swaathi K <swaathi@skcript.com>
2016-05-17 10:03:13 +0530

    Matching Redis keys

--- watchman/pigeon.py
+++ watchman/pigeon.py
@@ -3,7 +3,7 @@ import requests
 import logging
 
 # Modules in Watchman
-from conf import load_endpoints, LOG_FILENAME
+from conf import load_endpoints, LOG_FILENAME, REDIS
 
 ENDPOINTS = load_endpoints()
 
@@ -11,38 +11,51 @@ ENDPOINTS = load_endpoints()
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 log = logging.getLogger("watchman.pigeon")
 
+def check_path_processing(path):
+    path = path[:-1] if path[-1] == "/" else path
+    avl = REDIS.scan_iter(match="{0}/*".format(path))
+    return (sum(1 for _ in avl) > 0 or REDIS.get("migration") or REDIS.get("import") or REDIS.get("snapshot"))
+
 def post_folder_creation(src):
-    log.debug("post_folder_creation")
-    
-    options = { 'path': src }
-    requests.post(ENDPOINTS['folder_create'], params=options)
+    if check_path_processing(src):
+        log.debug("Not sending folder creation for {0}".format(src))
+    else:
+        log.debug("Sending folder creation for {0}".format(src))
+        options = { 'path': src }
+        requests.post(ENDPOINTS['folder_create'], params=options)
 
 def post_file_creation(src):
-    log.debug("post_file_creation")
-    
-    options = { 'path': src }
-    requests.post(ENDPOINTS['file_create'], params=options)
+    if check_path_processing(src):
+        log.debug("Not sending file creation for {0}".format(src))
+    else:
+        log.debug("Sending file creation for {0}".format(src))
+        options = { 'path': src }
+        requests.post(ENDPOINTS['file_create'], params=options)
 
 def post_folder_destroy(src):
-    log.debug("post_folder_destroy")
-    
+    log.debug("Removing folder at {0}".format(src))
+
     options = { 'path': src }
     requests.post(ENDPOINTS['folder_destroy'], params=options)
 
 def post_file_destroy(src):
-    log.debug("post_file_destroy")
-    
+    log.debug("Removing file at {0}".format(src))
+
     options = { 'path': src }
     requests.post(ENDPOINTS['file_destroy'], params=options)
 
 def post_folder_move(src, dest):
-    log.debug("post_folder_move")
-    
-    options = { 'oldpath': src, 'newpath': dest }
-    requests.post(ENDPOINTS['folder_move'], params=options)
+    if check_path_processing(src) or check_path_processing(dest):
+        log.debug("Not sending folder movement from {0} to {1}".format(src, dest))
+    else:
+        log.debug("Sending folder movement from {0} to {1}".format(src, dest))
+        options = { 'oldpath': src, 'newpath': dest }
+        requests.post(ENDPOINTS['folder_move'], params=options)
 
 def post_file_move(src, dest):
-    log.debug("post_file_move")
-    
-    options = { 'oldpath': src, 'newpath': dest }
-    requests.post(ENDPOINTS['file_move'], params=options)
+    if check_path_processing(src) or check_path_processing(dest):
+        log.debug("Not sending file movement from {0} to {1}".format(src, dest))
+    else:
+        log.debug("Sending file movement from {0} to {1}".format(src, dest))
+        options = { 'oldpath': src, 'newpath': dest }
+        requests.post(ENDPOINTS['file_move'], params=options)
