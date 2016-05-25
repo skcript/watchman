9fbf7ec56d12812596a9041df8877e066991c10f
Swaathi K <swaathi@skcript.com>
2016-05-25 10:29:45 +0530

    Cleaning up and adding ratelimit constants to conf

--- watchman/pigeon.py
+++ watchman/pigeon.py
@@ -6,28 +6,29 @@ import logging
 from rratelimit import Limiter
 
 # Modules in Watchman
-from conf import load_endpoints, LOG_FILENAME, REDIS
+from conf import load_endpoints, LOG_FILENAME, REDIS, RL_LIMIT, RL_PERIOD
 
 ENDPOINTS = load_endpoints()
-LIMITER = Limiter(REDIS, action='pigeon', limit=100, period=60)
+LIMITER = Limiter(REDIS, action='pigeon', limit=RL_LIMIT, period=RL_PERIOD)
 REGEX = re.compile("([a-zA-Z0-9_ -/]*)/active/home/(\w+)/uploads/(\d+)/hot_root/")
 
 # Logger Creds
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 log = logging.getLogger("watchman.pigeon")
 
-def RateLimited(maxPerSecond):
+def ratelimit():
     def decorate(func):
         def rateLimitedFunction(*args,**kargs):
             name = get_user_name(args[0])
             if name:
-                print name
                 while not LIMITER.checked_insert(name):
-                    print "sleeping"
+                    print "{0} is being rate limited".format(name)
                     time.sleep(0.05)
-                ret = func(*args,**kargs)
-                print "returning"
-                return ret
+            else:
+                print "Invalid args, {0}".format(args)
+
+            ret = func(*args,**kargs)
+            return ret
 
         return rateLimitedFunction
     return decorate
@@ -42,7 +43,7 @@ def check_path_processing(path):
     avl = REDIS.scan_iter(match="{0}*".format(path))
     return (sum(1 for _ in avl) > 0 or REDIS.get("migration") or REDIS.get("import") or REDIS.get("snapshot"))
 
-@RateLimited(1)
+@ratelimit()
 def post_folder_creation(src):
     if check_path_processing(src):
         log.debug("Not sending folder creation for {0}".format(src))
@@ -53,7 +54,7 @@ def post_folder_creation(src):
         options = { 'path': src }
         requests.post(ENDPOINTS['folder_create'], params=options)
 
-@RateLimited(1)
+@ratelimit()
 def post_file_creation(src):
     if check_path_processing(src):
         log.debug("Not sending file creation for {0}".format(src))
@@ -64,7 +65,7 @@ def post_file_creation(src):
         options = { 'path': src }
         requests.post(ENDPOINTS['file_create'], params=options)
 
-@RateLimited(1)
+@ratelimit()
 def post_folder_destroy(src):
     log.debug("Removing folder at {0}".format(src))
     print("Removing folder at {0}".format(src))
@@ -72,7 +73,7 @@ def post_folder_destroy(src):
     options = { 'path': src }
     requests.post(ENDPOINTS['folder_destroy'], params=options)
 
-@RateLimited(1)
+@ratelimit()
 def post_file_destroy(src):
     log.debug("Removing file at {0}".format(src))
     print("Removing file at {0}".format(src))
@@ -80,7 +81,7 @@ def post_file_destroy(src):
     options = { 'path': src }
     requests.post(ENDPOINTS['file_destroy'], params=options)
 
-@RateLimited(1)
+@ratelimit()
 def post_folder_move(src, dest):
     if check_path_processing(src) or check_path_processing(dest):
         log.debug("Not sending folder movement from {0} to {1}".format(src, dest))
@@ -91,7 +92,7 @@ def post_folder_move(src, dest):
         options = { 'oldpath': src, 'newpath': dest }
         requests.post(ENDPOINTS['folder_move'], params=options)
 
-@RateLimited(1)
+@ratelimit()
 def post_file_move(src, dest):
     if check_path_processing(src) or check_path_processing(dest):
         log.debug("Not sending file movement from {0} to {1}".format(src, dest))
