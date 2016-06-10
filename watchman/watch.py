c5f97d23faefde0efd6c7b087baf26a2feec7548
Swaathi K <swaathi@skcript.com>
2016-06-10 16:16:07 +0530

    Not allowing hidden paths (dot files) to be registered

--- watchman/watch.py
+++ watchman/watch.py
@@ -15,103 +15,96 @@ import watchman.pigeon
 from conf import load_paths, REDIS
 
 class FileWatch(RegexMatchingEventHandler):
-  queue = Queue('filewatcher', connection=REDIS)
+    queue = Queue('filewatcher', connection=REDIS)
 
-  def start(self):
-    self.observer = Observer()
-    for path in load_paths():
-        print path
-        self.observer.schedule(self, path=path, recursive=True)
-    self.observer.start()
-    print "* Running Watchman (Press CTRL+C to quit)"
+    def start(self):
+        self.observer = Observer()
+        for path in load_paths():
+            print path
+            self.observer.schedule(self, path=path, recursive=True)
+        self.observer.start()
+        print "* Running Watchman (Press CTRL+C to quit)"
 
-    try:
-      while True:
-        time.sleep(1)
-    except KeyboardInterrupt:
-      self.observer.stop()
-    self.observer.join()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
 
-  def stop(self):
-    self.observer.stop()
+    def stop(self):
+        self.observer.stop()
 
-  def on_any_event(self):
-    # Events with hidden paths (dot files) are not registered
-    if self.is_hidden(event.src_path):
-        return
+    def on_created(self, event):
+        src = event.src_path
+        if self.is_hidden(src):
+            # Events with hidden paths (dot files) are not registered
+            return
 
-  def on_created(self, event):
-    try:
-      src = event.src_path
+        try:
+            if event.is_directory:
+                print "Created folder {0}".format(src)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_folder_creation',
+                    src
+                )
+            else:
+                print "Created file {0}".format(src)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_file_creation',
+                    src
+                )
+        except Exception, e:
+            print(e)
 
-      if event.is_directory:
-        print "Created folder {0}".format(src)
-        FileWatch.queue.enqueue(
-            'watchman.pigeon.post_folder_creation',
-            src
-        )
-      else:
-        print "Created file {0}".format(src)
-        FileWatch.queue.enqueue(
-            'watchman.pigeon.post_file_creation',
-            src
-        )
-    except Exception, e:
-      print(e)
+    def on_deleted(self, event):
+        src = event.src_path
+        if self.is_hidden(src):
+            # Events with hidden paths (dot files) are not registered
+            return
 
-  def on_deleted(self, event):
-    try:
-      src = event.src_path
+        try:
+            if event.is_directory:
+                print "Deleted folder {0}".format(src)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_folder_destroy',
+                    src
+                )
+            else:
+                print "Deleted file {0}".format(src)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_file_destroy',
+                    src
+                )
 
-      if event.is_directory:
-        print "Deleted folder {0}".format(src)
-        FileWatch.queue.enqueue(
-          'watchman.pigeon.post_folder_destroy',
-          src
-        )
-      else:
-        print "Deleted file {0}".format(src)
-        FileWatch.queue.enqueue(
-          'watchman.pigeon.post_file_destroy',
-          src
-        )
+        except Exception, e:
+            print(e)
 
-    except Exception, e:
-      print(e)
+    def on_moved(self, event):
+        src = event.src_path
+        dest = event.dest_path
+        if self.is_hidden(src) or self.is_hidden(dest):
+            # Events with hidden paths (dot files) are not registered
+            return
 
-  def on_moved(self, event):
-    try:
-      src = event.src_path
-      dest = event.dest_path
+        try:
+            if event.is_directory:
+                print "Moved folder from {0} to {1}".format(src, dest)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_folder_move',
+                    src,
+                    dest
+                )
+            else:
+                print "Moved file from {0} to {1}".format(src, dest)
+                FileWatch.queue.enqueue(
+                    'watchman.pigeon.post_file_move',
+                    src,
+                    dest
+                )
+        except Exception, e:
+            print(e)
 
-      if event.is_directory:
-        print "Moved folder from {0} to {1}".format(src, dest)
-        FileWatch.queue.enqueue(
-          'watchman.pigeon.post_folder_move',
-          src,
-          dest
-        )
-      else:
-        print "Moved file from {0} to {1}".format(src, dest)
-        FileWatch.queue.enqueue(
-          'watchman.pigeon.post_file_move',
-          src,
-          dest
-        )
-    except Exception, e:
-      print(e)
-
-  def on_modified(self, event):
-    try:
-      src = event.src_path
-
-      if event.is_directory:
-        print "Modified folder {0}".format(src)
-      else:
-        print "Modified file {0}".format(src)
-    except Exception, e:
-        print(e)
-
-  def is_hidden(self, path):
-    m = re.search('(?<=\/\.)\w+', path)
-    return m is not None
+    def is_hidden(self, path):
+        m = re.search('(?<=\/\.)\w+', path)
+        return m is not None
